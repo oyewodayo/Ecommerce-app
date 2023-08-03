785e66ebb949bdb1aa9c3b65cbd4022a05aec7dd
Oyewo Temidayo <oyewodayo@gmail.com>
2023-08-03 19:08:11 +0100

    Added Custom Style and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ecommerce App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      
       home: const MainScreen(),
     );
   }
